b69439ada0742b378590a65570911d5011b73bae
Akheel51 <94372648+Akheel51@users.noreply.github.com>
2022-08-21 12:06:09 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,71 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | AkheelCalc   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+import os
+
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+
+def mul(n1, n2):
+    return n1 * n2
+
+
+def div(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+
+    print("  ".join(operations))
+
+    keep_calculating = 'c'
+    while keep_calculating == 'c':
+        operation_symbol = input("Pick an operation: ").strip()
+
+        num2 = float(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        num1 = answer
+
+        keep_calculating = input(
+            f"Type 'c' to continue calculating with {answer} or type 'n' to start a new calculation: ").lower()
+
+        if keep_calculating == 'n':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            calculator()
+
+
+calculator()
